8417019f49c283f4c186e7db668a5b164bd3c23c
SDM <emraud119@naver.com>
2025-09-09 23:10:58 +0900

    섹션3에 슬라이드 기능 추가 및 스타일 수정, 이미지 파일 추가

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
     
+        //섹션1의 1번 슬라이드  
         var swiper = new Swiper(".section1_1", {
             loop:true,
                 autoplay: {
@@ -16,6 +17,7 @@
                     clickable: true,
                 }
         });
+        //섹션1의 2번 슬라이드
         var swiper = new Swiper(".section1_2", {
                 navigation: {
                     nextEl: ".swiper-button-next",
@@ -28,6 +30,27 @@
                     clickable: true,
                 }
         });
+        //섹션3의 1번 슬라이드
+        // .swiper-slide {
+        //     flex-shrink: 0;
+        //     width: 100%;
+        //     height: 100%;
+        //     position: relative;
+        //     transition-property: transform;
+        //     display: block}
+        //섹션3의 2번 슬라이드 
+         var swiper = new Swiper(".section3_mid_2_2", {
+                loop:true,
+                slidesPerView: 3,
+                spaceBetween: 30,
+                allowTouchMove: false,
+                navigation: {
+                    nextEl: ".section3_circle2",
+                    prevEl: ".section3_circle1",
+                }
+        });
+
+
         //new
         var swiper = new Swiper(".new_item", {
             slidesPerView: 6,
